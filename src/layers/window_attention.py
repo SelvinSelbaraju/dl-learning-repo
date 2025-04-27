fe3b306e7e56ac65d014221eff2044bd3334eed4
Selvin Selbaraju <selvinoselbaraju@gmail.com>
2025-04-27 19:22:16 -0400

    add tests for window self attention

--- src/layers/window_attention.py
+++ src/layers/window_attention.py
@@ -10,6 +10,10 @@ class WindowSelfAttention(nn.Module):
 
     Parameters
     ----------
+    img_size: int
+        The expected H/W of patches this layer should receive.
+        For example, a 224x224 image with patch size 4 has 56x56 patches.
+        This argument should be 56 in this case.
     embedding_dim: int
         The dimension of each patch embedding coming into this layer.
         Different transformer blocks might have different values here due to patch merging.
@@ -25,15 +29,19 @@ class WindowSelfAttention(nn.Module):
     """
     def __init__(
         self,
+        img_size: int,
         embedding_dim: int,
         num_heads: int,
         attention_dropout_rate: float,
         proj_dropout_rate: float,
+        window_size: int = 2,
     ):
         super().__init__()
+        self.input_resolution = (img_size, img_size)
         self.embedding_dim = embedding_dim
         self.num_heads = num_heads
         self.head_dim = embedding_dim // num_heads
+        self.window_size = window_size
 
         # Layers
         self.softmax = nn.Softmax(-1)
@@ -46,15 +54,29 @@ class WindowSelfAttention(nn.Module):
         self.proj_dropout = nn.Dropout(proj_dropout_rate)
     
 
-    def _get_qkv(self, x: torch.Tensor) -> torch.Tensor:
+    def _project_qkv(self, x: torch.Tensor) -> torch.Tensor:
         """
-        Project the windows to QKV and reshape so its [Q,K,V] for each head
+        Project the original patch embeddings to 3C.
+        Separate method so that can test splitting into QKV works separately.
 
         Input: (B x H/M x W/M) x M**2 x C
+        Output: (B x H/M x W/M) x M**2 x 3C
+        """
+        return self.qkv(x)
+
+    
+    def _transform_qkv(self, qkv: torch.Tensor) -> torch.Tensor:
+        """
+        Given the projection to Q,K,V, reshape and permute.
+        This is so we have 3 tensors each with Q,K and V
+        Each of these has the NUM_HEADS representations.
+        3C represents C for Q, K and V respectively.
+        C is split NUM_HEADS times.
+
+        Input: (B x H/M x W/M) x M**2 x 3C
         Output: 3 x (B x H/M x W/M) x HEADS x M**2 x C/HEADS
         """
-        B_, N, C = x.shape()
-        qkv = self.qkv(x)
+        B_, N, C_ = qkv.shape
         return qkv.reshape(B_, N, 3, self.num_heads, self.head_dim).permute(2,0,3,1,4)
     
 
@@ -68,13 +90,16 @@ class WindowSelfAttention(nn.Module):
         """
         THREE, B_, HEADS, N, C_HEADS = qkv.shape
         q,k,v = qkv[0], qkv[1], qkv[2]
-        attention_weights = (q @ k.transpose(-2,-1)) * self.head_dim**-0.5
+        # Cheaper to divide all of Q first, instead of the attention weights
+        # Divide less numbers
+        q *= self.head_dim**-0.5
+        attention_weights = (q @ k.transpose(-2,-1))
         attention_weights = self.softmax(attention_weights)
         attention_weights = self.attention_dropout(attention_weights)
         # Need to put the head dim next to the embedding dim and flatten it.
         # I.e we need to get the updated representation in each head for each patch, together before we flatten it
         # The projection layer runs on a per patch basis.
-        return (attention_weights @ v).tranpose(1,2).view((B_, N, -1))
+        return (attention_weights @ v).transpose(1,2).reshape((B_, N, -1))
     
 
     def _linear_projection(self, x: torch.Tensor) -> torch.Tensor:
@@ -87,8 +112,6 @@ class WindowSelfAttention(nn.Module):
         x = self.proj(x)
         return self.proj_dropout(x)
 
-
-
     
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -96,10 +119,49 @@ class WindowSelfAttention(nn.Module):
         Output: (B x H/M x W/M) x M**2 x C
         """
         # N === M**2 here
-        B_, N, C = x.shape()
-        qkv = self._get_qkv(x)
+        B_, N, C = x.shape
+        qkv = self._project_qkv(x)
+        qkv = self._transform_qkv(qkv)
         x = self._self_attention(qkv)
         x = self._linear_projection(x)
         return x
+    
 
+    def flops(self) -> int:
+        """
+        Calculate the FLOPs for this layer for batch size 1.
+        The FLOPs are made up of:
+        1. Projecting from C to 3C
+        2. Dividing Q by a constant 
+        3. Multiplying Q and K transpose
+        4. Multiping QK_T with V
+        5. Projecting from C to C to aggregate head information
+
+        Softmax is ommitted here.
+        """
+        # Every patch has an embedding (H * W patches)
+        # Each of these goes from C with a bias to 3C
+        qkv_proj = self.input_resolution[0] * self.input_resolution[1] * (self.embedding_dim+1) * (self.embedding_dim*3)
+        # Divide Q, which has total dimension C for each patch embedding
+        divide_q = self.input_resolution[0] * self.input_resolution[1] * self.embedding_dim
+        # There are HW/M**2 windows
+        # Each window has M**2 projections of dimension C for Q and K 
+        # A single window for one head is (M**2 x C//NUM_HEADS) x (C//NUM_HEADS x M**2)
+        single_qkt = self.window_size**2 * self.embedding_dim//self.head_dim * self.window_size**2
+        # Dividing all the elements by a constant
+        single_qkt += single_qkt
+        # Multiple qkt with v for a single head
+        # That is (M**2 x M**2) x (M**2 * C//NUM_HEADS)
+        single_qkt_v = self.window_size**2 * self.window_size**2 * self.embedding_dim//self.head_dim
+        # The projection runs for every patch embedding to the same dimension (uses a bias too)
+        final_proj = self.input_resolution[0] * self.input_resolution[1] * (self.embedding_dim+1) * (self.embedding_dim)
+        return int(
+            qkv_proj +
+            divide_q +
+            self.num_heads * self.input_resolution[0] * self.input_resolution[1] / self.window_size**2 * (single_qkt + single_qkt_v) + 
+            final_proj
+        )
+        
+
+    
 
