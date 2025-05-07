c4cf59e8ef7dcf05abaf1e2a15845cdce27f97b6
Selvin Selbaraju <selvinoselbaraju@gmail.com>
2025-05-07 16:41:27 -0400

    add validation

--- src/trainers/lightning_trainer.py
+++ src/trainers/lightning_trainer.py
@@ -15,6 +15,7 @@ class LightningTrainer(L.LightningModule):
         super().__init__()
         self.model = model
         self.loss = nn.CrossEntropyLoss()
+        self.validation_step_outputs = []
     
 
     def training_step(self, batch: Tuple[torch.Tensor, torch.Tensor]) -> torch.Tensor:
@@ -26,6 +27,29 @@ class LightningTrainer(L.LightningModule):
         loss = self.loss(outputs, labels)
         self.log("train_loss", loss)
         return loss
+
+
+    def validation_step(self, batch):
+        x, labels = batch
+        outputs = self.model(x)
+        loss = self.loss(outputs, labels)
+        top1_class = torch.argmax(outputs, 1)
+        equality = torch.eq(top1_class, labels).to(torch.float32)
+        self.validation_step_outputs.append({"accuracy": equality, "loss": loss})
+
+
+    def on_validation_epoch_end(self):
+        accuracy = 0
+        loss = 0
+        length = 0
+        for metrics in self.validation_step_outputs:
+            accuracy += metrics["accuracy"].sum()
+            loss += metrics["loss"].sum()
+            length += metrics["accuracy"].shape[0]
+        self.log("val_loss", loss / length)
+        self.log("val_top1_accuracy", accuracy / length)
+        self.validation_step_outputs = []
+        
 
 
     def configure_optimizers(self) -> optim.Optimizer:
